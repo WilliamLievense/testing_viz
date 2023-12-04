5a68c7a952243f22787d67efd3d836bc05bdbea9
William Lievense <wplievense@hotmail.com>
2023-12-04 22:07:51 +0000

    final project

--- visualization.py
+++ visualization.py
@@ -8,12 +8,12 @@ fig = go.Figure(data=go.Choropleth(
     locations=df['code'], # Spatial coordinates
     z = df['number students'].astype(float), # Data to be color-coded
     locationmode = 'USA-states', # set of locations match entries in `locations`
-    colorscale = 'Blues',
-    colorbar_title = "color scale title",
+    colorscale = 'Greens',
+    colorbar_title = "COLORBAR OH YEA",
 ))
 
 fig.update_layout(
-    title_text = 'title here',
+    title_text = 'Wait its a map?',
     geo_scope='usa', # limite map scope to USA
 )
 
